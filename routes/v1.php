2daf729e039aba3dbcf4027ac149e7c05969fe91
Jewel Rana <jewelrana.dev@gmail.com>
2022-10-31 12:22:25 +0600

    feat (ECS-15) request rate limit

--- routes/v1.php
+++ routes/v1.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\API\V1\ApiAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'auth', 'middleware' => ['guest', 'throttle:5,1']], function() {
+Route::group(['prefix' => 'auth', 'middleware' => ['guest', 'throttle:10,1']], function() {
     Route::post('login', [ApiAuthController::class, 'login']);
 });
 
