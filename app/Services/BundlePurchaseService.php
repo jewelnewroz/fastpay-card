6d2d3b7dbd303747e35e846ddfe3135d32038054
Jewel Rana <jewelrana.dev@gmail.com>
2022-11-07 17:44:06 +0600

    feat (ECS-25) Working on bundle purchase

---
+++ app/Services/BundlePurchaseService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Services;
+
+use App\Gateways\GatewayInterface;
+use App\Helper\ResponseHelper;
+use App\Http\Requests\BundlePurchaseExecuteRequest;
+use App\Http\Requests\BundleValidationRequest;
+use App\Http\Traits\JsonResponseTrait;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
+
+class BundlePurchaseService
+{
+    use JsonResponseTrait;
+
+    private GatewayInterface $gateway;
+
+    public function __construct(GatewayInterface $gateway)
+    {
+        $this->gateway = $gateway;
+    }
+
+    public function validate(BundleValidationRequest $request): JsonResponse
+    {
+        try {
+            return $this->gateway->validate($request);
+        } catch (\Exception $exception) {
+            Log::error("BUNDLE_PURCHASE_VALIDATION_ERROR: " . $exception);
+            return response()->json(ResponseHelper::failed('Server error!'));
+        }
+    }
+
+    public function execute(BundlePurchaseExecuteRequest $request): JsonResponse
+    {
+        try {
+            $data = $this->gateway->execute($request);
+            return response()->json(ResponseHelper::success('Your purchase successful', $data));
+        } catch (\Exception $exception) {
+            Log::error("BUNDLE_PURCHASE_FAILED: " . $exception);
+            return response()->json(ResponseHelper::failed('Server error!'));
+        }
+    }
+}
